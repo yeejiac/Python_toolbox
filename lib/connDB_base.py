20e162c0fb5d88caca4e713fe8c164484bf97220
unknown <yeejiac@gmail.com>
2020-09-08 20:22:25 +0800

    add database connection

--- lib/connDB_base.py
+++ lib/connDB_base.py
@@ -5,12 +5,23 @@ parent_dir = os.path.dirname(sys.path[0])
 sys.path.insert(0, parent_dir)
 from database import *
 from lib.logwriter import *
+from lib.iniparser import *
 import pandas as pd
 
 class ConnDB_base:
     def __init__(self, url):
-        self.db_helper = Database_connection(url)
+        self.db_agent = Database_connection(url)
+        # metadata = create_all(self.db_agent)
 
     def insertToframe(self, df, tablename):
-        df.to_sql(tablename, con=self.db_helper,if_exists='append')
+        df.to_sql(tablename, con=self.db_agent,if_exists='append')
+
+    def showTable(self):
+        sql = "SELECT * FROM TEST"
+        df = pd.read_sql(sql, con=self.db_agent.engine)
+        print(df)
+
+if __name__ == '__main__':
+    db = ConnDB_base(db_url)
+    db.showTable()
 
--- lib/iniparser.py
+++ lib/iniparser.py
@@ -2,11 +2,27 @@ import configparser
 import os
 import sys
 parent_dir = os.path.dirname(sys.path[0])
-sys.path.insert(0, parent_dir) 
+sys.path.insert(0, parent_dir)
+import psycopg2
 
 config = configparser.ConfigParser()
 config.read('../doc/settings.ini')
 addr = config['socket']['addr']
 port = int(config['socket']['port'])
+
+
+
+# maxdb = psycopg2.connect(
+#   user=config['database']['user'],
+#     password=config['database']['password'],
+#     host=config['database']['addr'],
+#     port=int(config['database']['port']),
+#     database=config['database']['database'],
+#   )
+# cursor=maxdb.cursor()
+
+db_url = 'mysql://root:Aa1234@localhost:3306/my_db'
+
+
 
 
